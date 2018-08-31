97c105fd66ffdef4104035e0297f5936877e719e
Yezhel Marcial Ruiz <yezhelmr@gmail.com>
2018-08-31 00:02:14 -0500

    Trigger para actualizar el stock

---
+++ extras/triggerStock.php
@@ -0,0 +1,8 @@
+DELIMITER //
+CREATE TRIGGER tr_updStockIngreso AFTER INSERT ON detalle_ingresos
+ FOR EACH ROW BEGIN
+ UPDATE articulos SET stock = stock + NEW.cantidad 
+ WHERE articulos.id = NEW.idarticulo;
+END
+//
+DELIMITER ;
